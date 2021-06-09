9f3702895c79da00126816d56a41abd34f81ea3f
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-06-09 15:52:31 +0200

    before

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 const hamburger = document.querySelector('.hamburger');
 const sidebar = document.querySelector('.sidebar');
-const ul = document.querySelector('ul');
-const imRed = document.querySelector('.im-red');
 
 hamburger.addEventListener('click', function() {
-    // sidebar.classList.add('show-sidebar');
-    imRed.classList.toggle('red');
+    sidebar.classList.toggle('show-sidebar');
 });
