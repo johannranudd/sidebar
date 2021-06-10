e8cd5cf2bac6895ad346f2ce68fba5165ac79640
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-06-10 10:49:50 +0200

    added close button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const sidebar = document.querySelector('.sidebar');
+const closeBtn = document.querySelector('.closeBtn');
 
 hamburger.addEventListener('click', function() {
     sidebar.classList.toggle('show-sidebar');
+});
+
+closeBtn.addEventListener('click', function() {
+    sidebar.classList.remove('show-sidebar');
 });
